61bfc5bdfcc3d72be9cd4feb61385a9919591dfc
EBerk26 <eli_berk26@milton.edu>
2024-05-02 18:26:40 -0400

    Started the method Main.shuffle, which currently initalizes the deck of 52 cards.

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,20 @@
 public class Card {
     boolean inDeck = true; //is it discarded?
-    int value; //1-11
+    int value; //2-11
     String suit;
     String cardType; //number, jack, queen, king, ace
     public Card(){
 
+    }
+    void printInfo(){
+        if(value<10) {
+            System.out.println(value+" of "+suit);
+        } else{
+            if(cardType.equals("number")){
+                System.out.println("10 of "+suit);
+            } else{
+                System.out.println(cardType+" of "+suit);
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,48 @@
 public class Main {
+    Card[] deck = new Card[52];
     public Main() {
         System.out.println("Welcome to Eli's Casino");
+        for(int x = 0; x<=51; x++) {
+            deck[x] = new Card();
+        }
+        shuffle();
+        for(int x =0; x<=51; x++) {
+            deck[x].printInfo();
+        }
     }
     public static void main(String[] args) {
-        Main Main = new Main();
+        new Main();
+    }
+    void shuffle(){
+        for(int suits = 1; suits<=4;suits++){
+            for(int cardnumber = 1;cardnumber<=13;cardnumber++){
+                if(suits ==1) {
+                    deck[cardnumber - 1].suit="hearts";
+                } else if (suits == 2){
+                    deck[13 * (suits - 1) + cardnumber - 1].suit="spades";
+                } else if (suits == 3){
+                    deck[13 * (suits - 1) + cardnumber - 1].suit="diamonds";
+                }else{
+                    deck[13 * (suits - 1) + cardnumber - 1].suit="clubs";
+                }
+                if(cardnumber>1&&cardnumber<11) {
+                    deck[13 * (suits - 1) + cardnumber - 1].value = cardnumber;
+                    deck[13 * (suits - 1) + cardnumber - 1].cardType = "number";
+                }
+                else if(cardnumber>10){
+                    deck[13 * (suits - 1) + cardnumber - 1].value = 10;
+                    if(cardnumber==11){
+                        deck[13 * (suits - 1) + cardnumber - 1].cardType = "jack";
+                    } else if(cardnumber==12) {
+                        deck[13 * (suits - 1) + cardnumber - 1].cardType = "queen";
+                    } else{
+                        deck[13 * (suits - 1) + cardnumber - 1].cardType = "king";
+                    }
+                } else{
+                    deck[13 * (suits - 1) + cardnumber - 1].cardType = "ace";
+                    deck[13 * (suits - 1) + cardnumber - 1].value = 11;
+                }
+            }
+        }
     }
 }
