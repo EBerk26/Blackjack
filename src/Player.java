db7db95f6c0e65217d87cd9f420a87fc936f741b
EBerk26 <eli_berk26@milton.edu>
2024-05-11 18:04:57 -0400

    HW for 5/13 - Player.printInfo now runs Card.printInfo for every card in the hand that has a nonzero value.

--- src/Player.java
+++ src/Player.java
@@ -10,6 +10,9 @@ public class Player {
 
     }
     public Player(){
+        for(int x =0;x<=9;x++){
+            hand[x] = new Card();
+        }
         name = "Eli";
         handValue = 0;
         isDealer = false;
@@ -22,5 +25,11 @@ public class Player {
             dealerString = "not ";
         }
         System.out.println(name+" has a hand value of "+handValue+" and is "+dealerString+"the dealer.");
+        System.out.println("His hand contains the following cards:");
+        for (int x = 0;x<hand.length;x++){
+            if(hand[x].value>0) {
+                hand[x].printInfo();
+            }
+        }
     }
 }
